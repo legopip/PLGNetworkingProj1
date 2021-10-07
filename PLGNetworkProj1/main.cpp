28c3a6c7306eb7a4da5d1909cc234bff4e3991fd
legopip <79173416+legopip@users.noreply.github.com>
2021-10-07 18:30:18 -0400

    basic setup, should work?

---
+++ PLGNetworkProj1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "Lucas is nice" << std::endl;
+	std::cout << "Pilip is also nice" << std::endl;
+	std::cout << "Gian is like the worst" << std::endl;
+	return 1;
+}
