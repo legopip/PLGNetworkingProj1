0e3ff091004505216c468d6b33f62fb0e1cfe229
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2021-10-21 18:33:26 -0400

    Added buffer functionality for short and string

--- PLGNetworkingProj1Lib/Buffer.cpp
+++ PLGNetworkingProj1Lib/Buffer.cpp
@@ -5,37 +5,166 @@
 
 #include "Buffer.h";
 
-Buffer::Buffer(size_t size) {
+Buffer::Buffer(size_t size) 
+{
 	buffer = std::vector<uint8_t>(size);
+	indexRead = 0;
+	indexWrite = 0;
 }
 
-void Buffer::writeUInt32LE(std::size_t index, int32_t value) {
-	buffer[index] = value;
-	buffer[index + 1] = value >> 8;
-	buffer[index + 2] = value >> 16;
-	buffer[index + 3] = value >> 24;
+//void Buffer::writeUInt32LE(std::size_t index, int32_t value) 
+//{
+//	buffer[index] = value;
+//	buffer[index + 1] = value >> 8;
+//	buffer[index + 2] = value >> 16;
+//	buffer[index + 3] = value >> 24;
+//}
+
+//void Buffer::writeUInt32LE(int32_t value) 
+//{
+//	//std::size_t index = 0;
+//	//for (; index < buffer.size(); index++) {
+//	//	buffer[index] = value >> index * 8;
+//	//}
+//
+//	buffer[indexWrite] = value;
+//	buffer[indexWrite + 1] = value >> 8;
+//	buffer[indexWrite + 2] = value >> 16;
+//	buffer[indexWrite + 3] = value >> 24;
+//
+//	indexWrite += 4;
+//}
+
+//uint32_t Buffer::readUInt32LE(std::size_t index) 
+//{
+//	uint32_t value = buffer[index];
+//	value |= buffer[index + 1] << 8;
+//	value |= buffer[index + 2] << 16;
+//	value |= buffer[index + 3] << 24;
+//	return value;
+//}
+
+//uint32_t Buffer::readUInt32LE() 
+//{
+//	//std::size_t index = 0;
+//	//uint32_t value = 0;
+//	//for (; index < buffer.size(); index++) {
+//	//	value  |= buffer[index] << index * 8;
+//	//}
+//	//return value;
+//}
+
+void Buffer::writeUInt32BE(std::size_t index, int32_t value) 
+{
+	buffer[index] = value >> 24;
+	buffer[index + 1] = value >> 16;
+	buffer[index + 2] = value >> 8;
+	buffer[index + 3] = value;
 }
 
-void Buffer::writeUInt32LE(int32_t value) {
-	std::size_t index = 0;
-	for (; index < buffer.size(); index++) {
-		buffer[index] = value >> index * 8;
-	}
+void Buffer::writeUInt32BE(int32_t value) 
+{
+	buffer[indexWrite] = value >> 24;
+	buffer[indexWrite + 1] = value >> 16;
+	buffer[indexWrite + 2] = value >> 8;
+	buffer[indexWrite + 3] = value;
+
+	indexWrite += 4;
 }
 
-uint32_t Buffer::readUInt32LE(std::size_t index) {
-	uint32_t value = buffer[index];
-	value |= buffer[index + 1] << 8;
-	value |= buffer[index + 2] << 16;
-	value |= buffer[index + 3] << 24;
+uint32_t Buffer::readUInt32BE(std::size_t index) 
+{
+	uint32_t value = buffer[index] << 24;
+	value |= buffer[index + 1] << 16;
+	value |= buffer[index + 2] << 8;
+	value |= buffer[index + 3];
+
 	return value;
 }
 
-uint32_t Buffer::readUInt32LE() {
-	std::size_t index = 0;
-	uint32_t value = 0;
-	for (; index < buffer.size(); index++) {
-		value  |= buffer[index] << index * 8;
-	}
+uint32_t Buffer::readUInt32BE() 
+{
+	uint32_t value = buffer[indexRead] << 24;
+	value |= buffer[indexRead + 1] << 16;
+	value |= buffer[indexRead + 2] << 8;
+	value |= buffer[indexRead + 3];
+
+	indexRead += 4;
 	return value;
+}
+
+void Buffer::writeUInt16BE(std::size_t index, int16_t value)
+{
+	buffer[index] = value >> 8;
+	buffer[index + 1] = value;
+}
+
+void Buffer::writeUInt16BE(int16_t value)
+{
+	buffer[indexWrite] = value >> 8;
+	buffer[indexWrite + 1] = value;
+
+	indexWrite += 2;
+}
+
+uint16_t Buffer::readUInt16BE(std::size_t index)
+{
+	uint16_t value = buffer[index] << 8;
+	value |= buffer[index + 1];
+
+	return value;
+}
+
+uint16_t Buffer::readUInt16BE()
+{
+	uint16_t value = buffer[indexRead] << 8;
+	value |= buffer[indexRead + 1];
+
+	indexRead += 2;
+	return value;
+}
+
+void Buffer::writeUInt8BE(std::size_t index, std::string value)
+{
+	int charOrder = 0;
+	for (char c : value)
+	{
+		buffer[index + charOrder] = c;
+		charOrder++;
+	}
+}
+
+void Buffer::writeUInt8BE(std::string value)
+{
+	int charOrder = 0;
+	for (char c : value)
+	{
+		buffer[indexWrite + charOrder] = c;
+		charOrder++;
+	}
+
+	indexWrite += charOrder;
+}
+
+std::string Buffer::readUInt8BE(std::size_t index, int stringSize)
+{
+	std::string output;
+	for (int i = 0; i < stringSize; i++)
+	{
+		output.push_back(buffer[index + i]);
+	}
+
+	return output;
+}
+
+std::string Buffer::readUInt8BE(int stringSize)
+{
+	std::string output = "";
+	for (int i = 0; i < stringSize; i++)
+	{
+		output.push_back(buffer[indexRead + i]);
+		indexRead++;
+	}
+
+	return output;
 }
--- PLGNetworkingProj1Lib/Buffer.h
+++ PLGNetworkingProj1Lib/Buffer.h
@@ -7,15 +7,35 @@
 #include <cstdint>
 #include <cstddef>
 #include <vector>
+#include <string>
 
 //size_t size
+
+// int32_t -> int
+// int16_t -> short
+// int8_t -> char
+
 class Buffer {
-public:
+private:
+	int indexRead;
+	int indexWrite;
 	std::vector<uint8_t> buffer;
 
+public:
 	Buffer(std::size_t size);
-	void writeUInt32LE(std::size_t index, int32_t value);
-	void writeUInt32LE(int32_t value);
-	uint32_t readUInt32LE(std::size_t index);
-	uint32_t readUInt32LE();
+
+	void writeUInt32BE(std::size_t index, int32_t value);
+	void writeUInt32BE(int32_t value);
+	uint32_t readUInt32BE(std::size_t index);
+	uint32_t readUInt32BE();
+
+	void writeUInt16BE(std::size_t index, int16_t value);
+	void writeUInt16BE(int16_t value);
+	uint16_t readUInt16BE(std::size_t index);
+	uint16_t readUInt16BE();
+
+	void writeUInt8BE(std::size_t index, std::string value);
+	void writeUInt8BE(std::string value);
+	std::string readUInt8BE(std::size_t index, int stringSize);
+	std::string readUInt8BE(int stringSize);
 };
